315c1baec304e44e6fe403cc8426de2a0709b34b
Bill Thompson <bthompson@easypost.com>
2024-02-07 15:25:16 +0000

    add <leader>wi to increase split width

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>wi", ":vertical resize +20<CR>", { desc = "Increase the split's width" })
